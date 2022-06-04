3197d5e7cd6153c972833f3cad9e6e8eacc3802d
mcoggins96 <51301232+mcoggins96@users.noreply.github.com>
2022-06-04 15:08:28 +0100

    Update Shors algorithm.py
    Updated Shor's Algorithm to work with the latest version of Qiskit

--- Advanced/Shors algorithm.py
+++ Advanced/Shors algorithm.py
@@ -1,8 +1,9 @@
 from qiskit import IBMQ
-from qiskit.aqua import QuantumInstance
-from qiskit.aqua.algorithms import Shor
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import Shor
 
-IBMQ.enable_account('ENTER API TOKEN HERE') # Enter your API token here
+
+IBMQ.enable_account('ENTER API KEY HERE') # Enter your API token here
 provider = IBMQ.get_provider(hub='ibm-q')
 
 backend = provider.get_backend('ibmq_qasm_simulator') # Specifies the quantum device
@@ -11,10 +12,10 @@ print('\n Shors Algorithm')
 print('--------------------')
 print('\nExecuting...\n')
 
-factors = Shor(21) #Function to run Shor's algorithm where 21 is the integer to be factored
+factors = Shor(QuantumInstance(backend, shots=100, skip_qobj_validation=False)) 
 
-result_dict = factors.run(QuantumInstance(backend, shots=1, skip_qobj_validation=False))
-result = result_dict['factors'] # Get factors from results
+result_dict = factors.factor(N=21, a=2)
+result = result_dict.factors
 
 print(result)
 print('\nPress any key to close')
